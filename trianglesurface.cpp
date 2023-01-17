0fe65b4c2285393b5eed643a45c121f02411f388
Thomasdalsae <Thomasdalsae@gmail.com>
2023-01-17 21:06:37 +0100

    I belive something happend

--- trianglesurface.cpp
+++ trianglesurface.cpp
@@ -17,12 +17,43 @@ TriangleSurface::TriangleSurface() : VisualObject()
 
 TriangleSurface::~TriangleSurface(){}
 
+void TriangleSurface::init(GLint matrixUniform){
+    mMatrixUniform = matrixUniform;
+
+   initializeOpenGLFunctions();
+
+   //Vertex Array Object - VAO
+   glGenVertexArrays( 1, &mVAO );
+   glBindVertexArray( mVAO );
+
+   //Vertex Buffer Object to hold vertices - VBO
+   glGenBuffers( 1, &mVBO );
+   glBindBuffer( GL_ARRAY_BUFFER, mVBO );
+
+   glBufferData( GL_ARRAY_BUFFER, mVertices.size()*sizeof( Vertex ), mVertices.data(), GL_STATIC_DRAW );
+
+   // 1rst attribute buffer : vertices
+   glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+   glVertexAttribPointer(0, 3, GL_FLOAT,GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+   glEnableVertexAttribArray(0);
+
+   // 2nd attribute buffer : colors
+   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,  sizeof( Vertex ),  (GLvoid*)(3 * sizeof(GLfloat)) );
+   glEnableVertexAttribArray(1);
+
+   //enable the matrixUniform
+   // mMatrixUniform = glGetUniformLocation( matrixUniform, "matrix" );
+
+   glBindVertexArray(0);
+}
 
 TriangleSurface::TriangleSurface(std::string filnavn) : VisualObject()
 {
    readFile(filnavn);
    mMatrix.setToIdentity();
 }
+
+
 
 void TriangleSurface::readFile(std::string filnavn) {
    std::ifstream inn;
@@ -39,6 +70,12 @@ void TriangleSurface::readFile(std::string filnavn) {
        }
        inn.close();
    }
+}
+
+void TriangleSurface::draw(){
+   glBindVertexArray( mVAO );
+   glUniformMatrix4fv( mMatrixUniform, 1, GL_FALSE, mMatrix.constData());
+   glDrawArrays(GL_LINES, 0, mVertices.size());
 }
 
 
