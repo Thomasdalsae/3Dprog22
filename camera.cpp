53ca68957904497c9c445d1ec06d79e7f5ba3bb8
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-02-05 18:08:05 +0100

    Moved translate into camera class

--- camera.cpp
+++ camera.cpp
@@ -11,8 +11,8 @@ void Camera::init(GLint pMatrixUniform, GLint vMatrixUniform)
 
     mPmatrix.setToIdentity();
     mVmatrix.setToIdentity();
-mPmatrixUniform = pMatrixUniform;
-mVmatrixUniform = vMatrixUniform;
+    mPmatrixUniform = pMatrixUniform;
+    mVmatrixUniform = vMatrixUniform;
 }
 
 void Camera::perspective(int degrees, double aspect, double nearplane, double farplane)
@@ -31,4 +31,9 @@ void Camera::update()
     glUniformMatrix4fv(mPmatrixUniform,1,GL_FALSE,mPmatrix.constData()); // if using our own matrices use GL_TRUE INSTEAD
     glUniformMatrix4fv(mVmatrixUniform,1,GL_FALSE,mVmatrix.constData()); // if using our own matrices use GL_TRUE INSTEAD
 
+}
+
+void Camera::translate(float dx, float dy, float dz)
+{
+    mEye + QVector3D(dx, dy, dz);
 }
--- camera.h
+++ camera.h
@@ -13,6 +13,7 @@ public:
     void perspective (int degrees, double aspect, double nearplane, double farplane);
     void lookAt(const QVector3D& eye, const QVector3D& at, const QVector3D& up);
     void update();
+    void translate(float dx, float dy, float dz);
 private:
     GLint mPmatrixUniform;  //OPenGL reference to the uniform in the shader program
     GLint mVmatrixUniform;
